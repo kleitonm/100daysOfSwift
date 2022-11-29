6d0a54a5e70abaa2137db83d0c0b663724986c2f
Kleiton Mendes <kleiton.mendes@foursys.com.br>
2022-11-28 21:51:39 -0300

    First steps in Swift, first day #100DaysOfSwift

---
+++ 100DaysOfSwift.playground/Pages/Day-1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,42 @@
+import UIKit
+
+//First steps in Swift
+
+// 1. Variable
+var srt = "The last of Us"
+
+//2. Strings and integers
+var age = 38
+var population = 8_000_000
+var meaningOfLife = 42
+//meaningOfLife = "Forty two"
+
+
+//3. Multi-line strings
+var str1 = """
+This goes
+over multiple
+lines
+"""
+
+//4. Doubles and Booleans
+var pi = 3.14
+var twoAnswear = true
+
+// 5. String interpolation
+var score = 100
+var str = "Your score was: \(score)"
+var results = "The test results are here: \(srt)"
+
+
+// 6. Constants: o valor não será modificado
+let taylor = "swift"
+
+//7. Type annotations: vc pode declarar uma variavel implicitamente ou explicitamente
+let algum: String = "Legado"
+let year: Int = 2022
+let height: Double = 1.90
+let emicidaRap: Bool = true
+
+// 9. Simple types: Summary
+
