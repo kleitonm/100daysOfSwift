1800afff779de25765a233646cd317adb49eee69
Kleiton Mendes <kleiton_mendes@hotmail.com>
2023-04-27 13:42:19 -0300

    Iniciando projeto WeSplit em SwiftUI Day16

--- 100DaysOfSwiftUI.playground/Pages/Day-16.xcplaygroundpage/Contents.swift
+++ 100DaysOfSwiftUI.playground/Pages/Day-16.xcplaygroundpage/Contents.swift
@@ -1,7 +1,24 @@
 //: [Previous](@previous)
 
-import Foundation
+import SwiftUI
 
-var greeting = "Hello, playground"
+/*
+ Dias 16-24: Iniciando o SwiftUI
+ Agora que você dominou o básico do Swift, nestes dias você começará a criar aplicativos SwiftUI reais usando o que aprendeu.
+*/
 
-//: [Next](@next)
+// Projeto 1, primeira parte
+
+// WeSplit
+/*
+ Neste projeto, vamos construir um aplicativo de divisão de cheques que você pode usar depois de comer em um restaurante - você insere o custo de sua comida, seleciona quanto de gorjeta deseja deixar e quantas pessoas você re com, e ele vai te dizer quanto cada pessoa precisa pagar.
+ */
+
+/*
+ etapas:
+
+1 - Uma introdução prática a todas as técnicas que você aprenderá.
+2 - Um guia passo a passo para construir o projeto.
+3 - Desafios para você completar sozinho, para levar o projeto adiante.
+ */
+
